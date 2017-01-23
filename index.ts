68cfcfcde109cd8727345fbbb00e282ae8728bd2
Maxim Ivanov <omaxphp@yandex.ru>
2017-01-23 20:35:25 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 export enum Level {
-    DEBUG,
-    INFO,
-    WARNING,
-    ERROR
+    DEBUG = 1,
+    HELP = 2,
+    INFO = 3,
+    WARNING = 4,
+    ERROR = 5
 }
 
 export class Logger {
@@ -13,26 +14,66 @@ export class Logger {
         this.levelMin = level;
     }
 
-    
+    public debug(...args): Function {
+        if (this.levelMin > Level.DEBUG) return () => {};
+        return Logger.applyConsoleLog("color: blue;", "[DEBUG]", args);
+    }
+
+    public help(...args): Function {
+        if (this.levelMin > Level.DEBUG) return () => {};
+        return Logger.applyConsoleLog("color: green;", "[HELP]", args);
+    }
+
+    public info(...args): Function {
+        if (this.levelMin > Level.INFO) return () => {};
+        return Logger.applyConsoleLog("color: black;", "[INFO]", args);
+    }
+
+    public warning(...args): Function {
+        if (this.levelMin > Level.WARNING) return () => {};
+        return Logger.applyConsoleLog("color: #ef9b0f;", "[WARNING]", args);
+    }
+
+    public error(...args): Function {
+        if (this.levelMin > Level.ERROR) return;
+        return Logger.applyConsoleLog("color: red;", "[ERROR]", args);
+    }
 
     private static allElementOfString(element, index, array) {
         return typeof element === "string";
     }
 
     private static argsToString(arg) {
+        
         let argLine;
         let allString = arg.every(Logger.allElementOfString);
-        if (allString) argLine = arg;
-        else argLine = JSON.stringify(arg, null, 4);
+        
+        if (allString) {
+            argLine = arg;
+        } else {
+            try {
+                argLine = Logger.JSONStringify(arg);
+            } catch(e) {
+                console.warn("error in ts-debbugger", e);
+                argLine = arg
+            }
+            
+        }
+
         return argLine;
     }
 
     private static applyConsoleLog(color, label, args) {
         let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
         if (isChrome || !Logger.isIE()) {
+        
             let log = Logger.argsToString(args);
-            if (Array.isArray(log)) log = log.join(" ");
+            if (Array.isArray(log)) {
+                log = log.join(" ");
+            }
+        
             return window.console.log.bind(window.console, `%c${label} ${log}`, color)
+        
         } else {
             return window.console.log.bind(window.console, args);
         }
@@ -44,25 +85,25 @@ export class Logger {
         return msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./);
     }
 
-    public debug(...args): Function {
-        if (this.levelMin > Level.DEBUG) return () => {};
-        return Logger.applyConsoleLog("color: blue;", "[DEBUG]", args);
+    private static JSONStringify(object) {
+        let cache = [];        
+        let str = JSON.stringify(object,
+            // custom replacer fxn - gets around "TypeError: Converting circular structure to JSON" 
+            function(key, value) {
+                if (typeof value === 'object' && value !== null) {
+                    if (cache.indexOf(value) !== -1) {
+                        // Circular reference found, discard key
+                        return;
+                    }
+                    // Store value in our collection
+                    cache.push(value);
+                }
+                return value;
+            }, 4);
+        cache = null; // enable garbage collection
+        return str;
     }
 
-    public info(...args): Function {
-        if (this.levelMin > Level.INFO) return () => {};
-        return Logger.applyConsoleLog("color: black;", "[INFO]", args);
-    }
-
-    public warning(...args): Function {
-        if (this.levelMin > Level.WARNING) return () => {};
-        return Logger.applyConsoleLog("color: #eeac00;", "[WARNING]", args);
-    }
-
-    public error(...args): Function {
-        if (this.levelMin > Level.ERROR) return;
-        return Logger.applyConsoleLog("color: red;", "[ERROR]", args);
-    }
 
 }
 
